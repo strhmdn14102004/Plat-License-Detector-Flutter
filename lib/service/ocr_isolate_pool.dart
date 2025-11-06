3d8cf48ee950ab4b695108310b9ad0d77f597828
Satria Ramadan <strhmdn14102004@gmail.com>
2025-11-06 15:13:54 +0700

    Update ocr_isolate_pool.dart

--- lib/service/ocr_isolate_pool.dart
+++ lib/service/ocr_isolate_pool.dart
@@ -99,15 +99,15 @@ class OcrIsolatePool {
             .replaceAll('B', '8')
             .replaceAll('G', '6');
 
-        mid = mid.replaceAllMapped(RegExp(r'8'), (m) {
-          final i = m.start;
-          if (i > 0 && i < mid.length - 1) {
-            final before = mid[i - 1];
-            final after = mid[i + 1];
-            if ('906'.contains(before) || '906'.contains(after)) return '8';
-          }
-          return '3';
-        });
+        // mid = mid.replaceAllMapped(RegExp(r'8'), (m) {
+        //   final i = m.start;
+        //   if (i > 0 && i < mid.length - 1) {
+        //     final before = mid[i - 1];
+        //     final after = mid[i + 1];
+        //     if ('906'.contains(before) || '906'.contains(after)) return '8';
+        //   }
+        //   return '3';
+        // });
 
         suffix = suffix
             .replaceAll('0', 'O')
@@ -185,8 +185,7 @@ class OcrIsolatePool {
         'CD',
       ];
 
-      final plateRegex =
-          RegExp(r'^[A-Z]{1,3}[\s\-]?\d{1,5}[\s\-]?[A-Z]{0,4}$');
+      final plateRegex = RegExp(r'^[A-Z]{1,3}[\s\-]?\d{1,5}[\s\-]?[A-Z]{0,4}$');
       final plateWithTimeRegex = RegExp(
         r'^([A-Z]{1,3}\s?\d{1,5}\s?[A-Z]{0,4})[\s\n]+(\d{2}[:.,-]?\d{2})$',
       );
